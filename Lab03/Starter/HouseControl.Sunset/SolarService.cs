9125671f5d5d5281a0fb707adceb5bc33424460a
Jeremy Clark <jeremy@jeremybytes.com>
2020-08-16 18:18:44 -0700

    Added Lab03 files

---
+++ Lab03/Starter/HouseControl.Sunset/SolarService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net;
+
+namespace HouseControl.Sunset
+{
+    public interface ISolarService
+    {
+        string GetServiceData(DateTime date);
+    }
+
+    public class SolarService : ISolarService
+    {
+        WebClient client = new WebClient();
+        string baseUri = "http://localhost:8973/";
+
+
+        public string GetServiceData(DateTime date)
+        {
+            var address = $"{baseUri}api/SolarCalculator/33.7676/-84.5606/{date:yyyy-MM-dd}";
+            string reply = client.DownloadString(address);
+            return reply;
+        }
+    }
+}
---
+++ Lab03/Starter/HouseControl.Sunset/SolarServiceSunsetProvider.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace HouseControl.Sunset
+{
+    public class SolarServiceSunsetProvider : ISunsetProvider
+    {
+        private ISolarService service;
+
+        public ISolarService Service
+        {
+            get
+            {
+                if (service == null)
+                    service = new SolarService();
+                return service;
+            }
+            set { service = value; }
+        }
+
+        public DateTimeOffset GetSunrise(DateTime date)
+        {
+            string serviceData = Service.GetServiceData(date);
+            string sunriseTimeString = ParseSunriseTime(serviceData);
+            DateTime sunriseTime = ToLocalTime(sunriseTimeString, date);
+            return new DateTimeOffset(sunriseTime);
+        }
+
+        public DateTimeOffset GetSunset(DateTime date)
+        {
+            string serviceData = Service.GetServiceData(date);
+            string sunsetTimeString = ParseSunsetTime(serviceData);
+            DateTime sunsetTime = ToLocalTime(sunsetTimeString, date);
+            return new DateTimeOffset(sunsetTime);
+        }
+
+        public static DateTime ToLocalTime(string inputTime, DateTime date)
+        {
+            DateTime time = DateTime.Parse(inputTime);
+            DateTime result = date.Date + time.TimeOfDay;
+            return result;
+        }
+
+        public static string ParseSunsetTime(string jsonData)
+        {
+            dynamic data = JsonConvert.DeserializeObject(jsonData);
+            string sunsetTimeString = data.results.sunset;
+            return sunsetTimeString;
+        }
+
+        public static string ParseSunriseTime(string jsonData)
+        {
+            dynamic data = JsonConvert.DeserializeObject(jsonData);
+            return data.results.sunrise;
+        }
+    }
+}
